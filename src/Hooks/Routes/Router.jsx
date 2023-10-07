42a1948b94115962fcfb9845ef39c99033251d9e
shabnaz21 <shabnaz608@gmail.com>
2023-10-07 13:19:31 +0600

    4th Stage: Implement 404 error route

--- src/Hooks/Routes/Router.jsx
+++ src/Hooks/Routes/Router.jsx
@@ -1,13 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../../components/Layout/Root";
 import Home from "../../pages/Home/Home";
+import Error from "../../pages/ErrorPage/Error";
 
 
 
 const routes = createBrowserRouter([
     {
         path: '/',
-        element:<Root></Root>,
+        element: <Root></Root>,
+        errorElement: <Error></Error>,
         children: [
             {
                 path: '/',
